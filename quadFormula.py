26fc2279bc94de5f3420c6570343c05b8dd5a750
Parash <85245101+parashie@users.noreply.github.com>
2022-09-04 18:45:11 -0400

    Update quadFormula.py

--- quadFormula.py
+++ quadFormula.py
@@ -3,8 +3,6 @@ import cmath
 
 # This program will ask the user for the coefficient values of a quadratic equation
 # then it will solve the equation, and return the results to the user
-# Imaginary solutions may or may not work I dont really know
-# Hopefully this works
 # Really i made this because I failed my math test, and got inspired to re-learn the quadratic formula
 
 # roots, zeros and solutions will be used interchangeably throughout the comments
